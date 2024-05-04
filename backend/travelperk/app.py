22b9a39dcf8f0c1de541c0957217edc1d970f5a6
AbelJuncal <72970486+AbelJuncal@users.noreply.github.com>
2024-05-04 11:24:00 +0200

    Cositas

--- backend/travelperk/app.py
+++ backend/travelperk/app.py
@@ -1,4 +1,7 @@
 import flask
+from models.person import Person
+import firebase_admin
+from firebase_admin import db
 
 def create_app():
 
@@ -8,7 +11,26 @@ def create_app():
 
 app = create_app()
 
+def connect_to_database():
+    databaseURL = 'https://hackudc-49b6e-default-rtdb.europe-west1.firebasedatabase.app/'
+
+    cred_obj = firebase_admin.credentials.Certificate('./key.json')
+    default_app = firebase_admin.initialize_app(cred_obj, {
+        'databaseURL':databaseURL
+        })
+    
+database = connect_to_database()
+
 @app.route('/', methods=['GET'])
 def obtener_dato():
     return 'Hello, World!'
+
+@app.route('/users/<username>', methods=['GET'])
+def obtener_usuario(username):
+    ref = db.reference('/User1')
+    print(ref.get('/'))
+    return f"User: {username}"
+    ref = db.reference('/User1')
+    print(ref.get('/'))
+    return
     
