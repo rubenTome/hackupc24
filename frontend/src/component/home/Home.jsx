0b818572bd2588a7b56fdbcd4d1b1e4af2aa755c
AntonCanzobre <anton.c744@gmail.com>
2024-05-04 03:42:43 +0200

    Trip

--- frontend/src/component/home/Home.jsx
+++ frontend/src/component/home/Home.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import logo from "../../assets/img/Logo_TravelPerk.png";
+import Trip from "../trip";
 
 
 const Home = () => {
     
     return (
-        <main >
-            <nav className="border-b border-black-500 drop-shadow-sm p-4">
-                <img src={logo} alt="Logo" 
-                className="w-48"/>
+        <main>
+            <nav className="border-b border-black-500 shadow-md p-4 flex items-center justify-between mb-10">
+                <img src={logo} alt="Logo" className="w-48"/>
+                
+                <div className="flex">
+                    <button className="text-white rounded-l-lg border  bg-blue-500 hover:bg-white hover:text-blue-500 focus:outline-none px-4 py-2">Trip</button>
+                    <button className="text-blue-500 rounded-r-lg border  bg-white hover:bg-blue-500 hover:text-white focus:outline-none px-4 py-2">Social</button>
+                </div>
 
-                <button className="g-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Trip</button>
-                <button>Social</button>
+                <div></div>
             </nav>
 
-
-            
+            <div className="flex justify-center">
+                <Trip />
+            </div>
         </main>
+
     );
 };
 
---
+++ frontend/src/component/trip/Trip.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../../assets/img/Logo_TravelPerk.png";
+
+
+const Trip = () => {
+    
+    return (
+        <div className="space-y-4">
+            <div className="flex">
+                <button className="rounded-l-lg border border-gray-300 bg-white hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-500 px-4 py-2">Avion</button>
+                <button className="border border-gray-300 bg-white hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-500 px-4 py-2">Tren</button>
+                <button className="rounded-r-lg border border-gray-300 bg-white hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-500 px-4 py-2">Coche</button>
+            </div>
+
+            <div>
+                <label htmlFor="salida" className="block font-medium text-gray-700">Salida: </label>
+                <input type="text" name="salida" id="salida" className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500" />
+                <input type="date" name="fechaSalida" id="fechaSalida" className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500 mx-5" />
+
+            </div>
+
+            <div>
+                <label htmlFor="destino" className="block font-medium text-gray-700">Destino:</label>
+                <input type="text" name="destino" id="destino" className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500" />
+                <input type="date" name="fechaLlegada" id="fechaLlegada" className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500 mx-5" />
+            </div>
+
+            <button className="bg-blue-500 g-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buscar</button>
+        </div>
+    );
+};
+
+export default Trip;
